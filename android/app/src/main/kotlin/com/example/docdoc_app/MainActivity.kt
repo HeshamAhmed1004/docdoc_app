efccf0fb7487bfa62a616837d5dc845f4dd5604d
HeshamAhmed1004 <heshamahmed1004@gmail.com>
2024-12-27 03:56:16 +0200

    add Production & Development flavor for Android

--- android/app/src/main/kotlin/com/example/docdoc_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/docdoc_app/MainActivity.kt
@@ -1,5 +1,18 @@
+
 package com.example.docdoc_app
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+      override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+          }
+}
+
+
+
